057bc2284313588201209a6cef6fe6b315bb972b
Dishan <dishanf@irononetech.com>
2023-03-26 19:12:49 +0530

    Add utility to save and image as a PNG file

--- src/main/kotlin/Util.kt
+++ src/main/kotlin/Util.kt
@@ -1,5 +1,13 @@
+import java.awt.image.RenderedImage
+import java.io.File
+import javax.imageio.ImageIO
+
 fun <T> MutableCollection<T>.addIfNotNull(value: T?) {
     if (value != null) {
         add(value)
     }
+}
+
+fun RenderedImage.saveAsPng(path: String) {
+    ImageIO.write(this, "png", File(path))
 }
