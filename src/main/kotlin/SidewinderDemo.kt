9aff0c090e5f88aaf642bc3480e9e1fc1a86902d
Dishan <dishanf@irononetech.com>
2023-03-26 19:13:28 +0530

    SidewinderDemo: update to output png image of the maze

--- src/main/kotlin/SidewinderDemo.kt
+++ src/main/kotlin/SidewinderDemo.kt
@@ -1,7 +1,8 @@
 fun main() {
-    repeat(4) {
-        val grid = Grid(4, 4)
+    repeat(1) {
+        val grid = Grid(32, 32)
         Sidewinder.on(grid)
         println(grid)
+        grid.toImage().saveAsPng("./mazes/sidewinder.png")
     }
 }
