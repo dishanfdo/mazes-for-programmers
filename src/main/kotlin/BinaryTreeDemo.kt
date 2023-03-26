01f986b6a88fe1248aeba6fc6d043bd19fe38597
Dishan <dishanf@irononetech.com>
2023-03-26 20:45:26 +0530

    BinaryTreeDemo: update to output png image of the maze

--- src/main/kotlin/BinaryTreeDemo.kt
+++ src/main/kotlin/BinaryTreeDemo.kt
@@ -1,5 +1,8 @@
 fun main() {
-    val grid = Grid(4, 4)
-    BinaryTree.on(grid)
-    println(grid)
+    repeat(1) {
+        val grid = Grid(32, 32)
+        BinaryTree.on(grid)
+        println(grid)
+        grid.toImage().saveAsPng("./mazes/binary_tree.png")
+    }
 }
