84456a1726cc7449c08b40dafa2d4baccc5d6f75
Dishan <dishanf@irononetech.com>
2023-03-27 15:08:05 +0530

    Add Coloring.kt class

---
+++ src/main/kotlin/Coloring.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val grid = ColoredGrid(25, 25)
+    BinaryTree.on(grid)
+
+    val start = grid[grid.rowCount / 2, grid.colCount / 2] ?: error("Start cell cannot be null")
+    grid.distances = start.distances()
+
+    grid.toImage().saveAsPng("./mazes/colorized.png")
+}
