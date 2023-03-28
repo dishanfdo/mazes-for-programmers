953e2864fc236b7382472b172dfb14095bdd6ebf
Dishan <dishanf@irononetech.com>
2023-03-28 11:26:56 +0530

    Add AldousBroderColored.kt

---
+++ src/main/kotlin/AldousBroderColored.kt
@@ -0,0 +1,11 @@
+fun main() {
+    repeat(6) { n ->
+        val grid = ColoredGrid(20, 20)
+        AldousBroder.on(grid)
+
+        val middle = grid[grid.rowCount / 2, grid.colCount / 2] ?: error("middle cell cannot be null")
+        grid.distances = middle.distances()
+
+        grid.toImage().saveAsPng("./mazes/aldous_broder_$n.png")
+    }
+}
