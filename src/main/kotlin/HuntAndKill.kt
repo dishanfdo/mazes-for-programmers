6ddfae8d2fd12aaab5ded1702f98d8c7d0bc528f
Dishan <dishanf@irononetech.com>
2023-03-28 14:36:13 +0530

    Implement Hunt-And-Kill algorithm

---
+++ src/main/kotlin/HuntAndKill.kt
@@ -0,0 +1,32 @@
+class HuntAndKill {
+    companion object {
+        fun on(grid: Grid) {
+            fun Cell.isVisited() = links.isNotEmpty()
+            fun Cell.isNotVisited() = !isVisited()
+            fun Cell.unvisitedNeighbours() = neighbours.filter { it.isNotVisited() }
+            fun Cell.visitedNeighbours() = neighbours.filter { it.isVisited() }
+
+            var current: Cell? = grid.randomCell()
+            while(current != null) {
+                val unvisitedNeighbours = current.unvisitedNeighbours()
+                if (unvisitedNeighbours.isNotEmpty()) {
+                    val neighbour = unvisitedNeighbours.random()
+                    current.link(neighbour)
+                    current = neighbour
+                } else {
+                    current = null
+
+                    for (cell in grid.cells) {
+                        val visitedNeighbours = cell.visitedNeighbours()
+                        if (cell.isNotVisited() && visitedNeighbours.isNotEmpty()) {
+                            current = cell
+                            val neighbour = visitedNeighbours.random()
+                            current.link(neighbour)
+                            break
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
